c530cae2a817511c9bcd0ebbd0fdb57ed73621c9
AqilJaafree <wanaqilre@gmail.com>
2024-07-10 01:05:11 +0800

    Bug: Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
+    const [nav, setNav] = useState(false);
+    
     const handleNav = () => {
-        setNav(!nav)
-    }
+        setNav(!nav);
+    };
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Learn Trail</h1>
@@ -18,21 +19,18 @@ const Navbar = () => {
       </ul>
       
       <div onClick={handleNav} className='block md:hidden'>
-        {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' :  'fixed left-[-100%]' }>
-
-      <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Learn Trail</h1>   
-      <ul className='uppercase p-4'>
-        <li className='p-4 border-b border-gray-600'>Home</li>
-        <li className='p-4 border-b border-gray-600'>Company</li>
-        <li className='p-4 border-b border-gray-600'>Resources</li>
-        <li className='p-4 border-b border-gray-600'>About</li>
-        <li className='p-4'>Context</li>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Learn Trail</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Context</li>
       </ul>
-      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
