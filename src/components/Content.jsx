97dc94d74abc06d75f4337a7c18b54cbda4cde02
AqilJaafree <wanaqilre@gmail.com>
2024-07-11 12:52:01 +0800

    Feat: Content added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,8 +6,8 @@ const Content = () => {
       <div className="max-w-[1280px] mx-auto grid md:grid-cols-2">
         <img className="max-w-[500px] mx-auto my-4" src={Guild} alt="/" />
         <div className="flex flex-col justify center">
-          <p >COURSE LEARNING GUILD</p>
-          <h1>All courses listed</h1>
+          <p className='text-[#00df9a] font-bold' >COURSE LEARNING GUILD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>All courses listed</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
             consectetur est rutrum bibendum viverra. Integer luctus nunc sed
@@ -17,6 +17,7 @@ const Content = () => {
             magna molestie pretium. Integer nec lectus laoreet, elementum ex
             eget, molestie risus.
           </p>
+          <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 '>Learn with us</button>
         </div>
       </div>
     </div>
